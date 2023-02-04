1ab668e8559ac4f3fcff23afe12bf755726295b8
aidanGoesch <aidangoeschel@gmail.com>
2023-02-04 15:49:10 -0800

    Changed the init method and added the distance method

--- knn.py
+++ knn.py
@@ -1,33 +1,53 @@
 import math
 
 K = 5
+
+
 class MetricsVector:
     def __init__(self, danceability: float, energy: float, m_key: int, loudness: float,
                  speechiness: float, acousticness: float, instrumentalness: float,
                  liveness: float, valence: float, tempo: float):
-        self.danceability = average_metric(danceability, 1)
-        self.energy = average_metric(energy, 1)
-        self.m_key = average_metric(m_key, 1)
-        self.loudness = average_metric(loudness, 7.234)
-        self.speechiness = average_metric(speechiness, 0.969)
-        self.acousticness = average_metric(acousticness, 0.996)
-        self.instrumentalness = average_metric(instrumentalness, 1)
-        self.liveness = average_metric(liveness, 1)
-        self.valence = average_metric(valence, 1)
-        self.tempo = average_metric(tempo, 248.934)
+        self.vector = (average_metric(danceability, 1),
+                       average_metric(energy, 1),
+                       average_metric(m_key, 1),
+                       average_metric(loudness, 7.234),
+                       average_metric(speechiness, 0.969),
+                       average_metric(acousticness, 0.996),
+                       average_metric(instrumentalness, 1),
+                       average_metric(liveness, 1),
+                       average_metric(valence, 1),
+                       average_metric(tempo, 248.934))
 
-    # getters and setters
+    def get_vector(self) -> tuple:
+        return self.vector
 
 
 def average_metric(metric: float, max_val: float | int) -> float:
-    return round(metric/max_val, 5)
+    return round(metric / max_val, 5)
+
 
 def distance_between(vec1: MetricsVector, vec2: MetricsVector) -> float:
-    pass
+    """gets the distance between 2 vectors"""
+    squared_total = 0
+    vec1_tuple = vec1.get_vector()
+    vec2_tuple = vec2.get_vector()
 
-def get_closest_k(vec_list: list[MetricsVector]) -> list[MetricsVector]:
+    for i in range(len(vec1_tuple)):
+        difference = vec2_tuple[i] - vec1_tuple[i]
+        squared_total += math.pow(difference, 2)
+
+    return abs(math.sqrt(squared_total))
+
+
+
+def get_closest_k(vec_list: list[MetricsVector], query: MetricsVector) -> list[MetricsVector]:
     """returns the closest K number of vectors to the average vector"""
     pass
 
 
+if __name__ == '__main__':
+    vector1 = MetricsVector(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    vector2 = MetricsVector(1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+    print(distance_between(vector1, vector2))
 
