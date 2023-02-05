2a9b493c0a8924ab3b554219af8afc6f54607382
aidanGoesch <aidangoeschel@gmail.com>
2023-02-05 01:51:22 -0800

    loweky finished main maybe?

--- main.py
+++ main.py
@@ -8,27 +8,37 @@ from flask_app import app
 import moods
 import pandas_reading
 import os
+import top_track_of_artist
 
 port = os.environ.get("PORT", 5001)
-def main():
-    mood = 'Happy'.lower()
-    artist = artist_csv
 
-    average_vector = moods.MOOD[mood]
+
+def main(artist: str, mood: str):
+    mood = mood.lower()
+
+    temp = top_track_of_artist.create_csv(artist)
 
     songs_csv = Path('somewhere')
 
-    song_list = pandas_reading.get_song_list(songs_csv)
-    artist_songs = pandas_reading.get_song_list(artist)
+    average_vector = moods.MOOD[mood]
 
-    artist_avg = knn.get_average_vector(artist_songs)
+    if temp != 'Something went wrong.':
+        artist_csv = Path('somewhere')
 
-    weighted_avg = knn.account_for_artist_vector(average_vector, artist_avg)
+        song_list = pandas_reading.get_song_list(songs_csv)
+        artist_songs = pandas_reading.get_song_list(artist_csv)
 
-    closest_songs = knn.get_closest_k(song_list, weighted_avg)
+        artist_avg = knn.get_average_vector(artist_songs)
 
+        weighted_avg = knn.account_for_artist_vector(average_vector, artist_avg)
 
+        closest_songs = knn.get_closest_k(song_list, weighted_avg)
+    else:
+        average_vector = moods.MOOD[mood]
+        song_list = pandas_reading.get_song_list(songs_csv)
+        closest_songs = knn.get_closest_k(song_list, average_vector)
 
+    return closest_songs
 
 
 if __name__ == '__main__':
